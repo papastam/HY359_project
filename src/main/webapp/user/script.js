a9b36e95eb6812dea435e12f43e0e098da684ffe
bisias <dimitrissmodz@gmail.com>
2022-01-10 21:11:44 +0200

    User/ check for blood donation done

--- src/main/webapp/user/script.js
+++ src/main/webapp/user/script.js
@@ -18,7 +18,39 @@ function loadmessages(){
     $("#appwindow").load("messages.html");
 }
 
+const excludedMessages = [];
+
+function checkForDonation() {
+    if(userdata["blooddonor"] !== 1) {
+        return;
+    }
+    var xhr = new XMLHttpRequest();
+
+    xhr.onload = function () {
+        if(xhr.status === 200 && xhr.readyState === 4) {
+            var reply = JSON.parse(xhr.responseText);
+            if(!(excludedMessages.includes(reply["message_id"]))) {
+                excludedMessages.push(reply["message_id"]);
+                for(let i in reply["messages"]) {
+                    var msg = JSON.parse(reply["messages"][i]);
+
+                    alert(msg["message"]);
+                }
+            }
+        }
+    }
+
+    xhr.open("GET", "/Project_war_exploded/MessagesAPI?bloodtype="+userdata["bloodtype"]);
+    xhr.send();
+}
+
+
+window.setInterval(function () {
+    checkForDonation();
+}, 3000);
+
 $(document).ready(function () {
     checkUserLoggedIn("user");
     loadinfo();
+
 });
